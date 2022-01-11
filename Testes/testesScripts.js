863b9a799988e590e54a76b53d383444ae2052fe
leonardo vicentin <leonardo.vicenin@gupy.com.br>
2022-01-10 21:57:21 -0300

    Add testes

---
+++ Testes/testesScripts.js
@@ -0,0 +1,97 @@
+const nome = 'Leonardo Vicentin'
+const idade = 25
+
+console.log(nome,idade)
+
+const title = 'Programador a bordo'
+
+console.log(title.length)
+
+console.log(title.toUpperCase())
+
+console.log(title.toLowerCase())
+
+console.log(title.indexOf('dor'))
+
+console.log(title.indexOf('treino'))
+
+console.log(title.replace('Programador', 'Desenvolvedor'))
+
+console.log(title.slice(3,8))
+
+const autor = 'Ayrton'
+
+console.log(`O canal do youtube ${title} foi criado por ${autor}`)
+
+//Operações Matematicas
+
+console.log(10 + 5 + 3)
+
+console.log(10 - 5 - 3)
+
+console.log(10 * 3)
+
+console.log(10 / 3)
+
+console.log(7 ** 2)
+
+var a = 10
+var b = 18
+
+console.log(a + b)
+
+var a = 1
+a = a + 1
+
+console.log(a)
+
+a++;
+console.log(a)
+console.log(a++)
+console.log(a)
+console.log(++a)
+console.log(--a)
+
+
+//booleans
+
+var a = true
+console.log(a)
+
+var b = false
+console.log(b)
+
+console.log('3 < 5', 3 < 5)
+console.log('3 > 5', 3 > 5)
+
+console.log('5 >= 5', 5 >= 5)
+console.log('5 >= 6', 5 >= 6)
+
+console.log('5 <= 7', 5 <= 7)
+
+console.log('10 != 10', 10 != 10)
+console.log('10 == 10', 10 == 10)
+console.log('5 == 4', 5 == 4)
+
+var nome1 = 'Leo'
+var nome2 = 'Pedro'
+
+console.log(nome1 == nome2)
+
+console.log(nome1 != nome2)
+
+console.log('4 === 4',4 == 4)
+console.log('4 === 4' , '4' === 4)
+
+// operadores logicos
+
+
+
+
+
+
+
+
+
+
+
